b3ea57fb113dea6803eb594f0c956c3e551b74a8
RealAllenDa <2153867950@qq.com>
2023-01-18 21:32:57 +0800

    shake_level: fix model parsing failed (#1)

--- model/shake_level.py
+++ model/shake_level.py
@@ -1,5 +1,7 @@
 __all__ = ["ShakeLevelReturnModel", "ShakeLevelApiModel"]
 
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -9,7 +11,7 @@ class ShakeLevelReturnModel(BaseModel):
     yellow: int
     red: int
     sync_time: str
-    status: int
+    status: Optional[int]
 
     class Config:
         allow_population_by_field_name = True
