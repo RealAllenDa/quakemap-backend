cb85a959b36b535d5a2237e937c69e121afb6603
RealAllenDa <2153867950@qq.com>
2024-01-22 16:55:43 +0800

    Database: Run migrations automatically

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,11 @@
-import asyncio
 import os
 import sys
 from logging.config import fileConfig
 
 from alembic import context
 from dotenv import load_dotenv
-from sqlalchemy import pool
+from sqlalchemy import pool, engine_from_config
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
 
 import models
 # noinspection PyUnresolvedReferences
@@ -30,7 +28,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"].replace("asyncpg", "psycopg2"))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -76,28 +74,26 @@ def do_run_migrations(connection: Connection) -> None:
         context.run_migrations()
 
 
-async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-
-    connectable = async_engine_from_config(
+    connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
-    await connectable.dispose()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
-
-    asyncio.run(run_async_migrations())
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 if context.is_offline_mode():
--- internal/db.py
+++ internal/db.py
@@ -1,12 +1,21 @@
 import os
 import sys
 
+from alembic import command
+from alembic.config import Config
 from loguru import logger
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 from sdk import verify_not_used
+
+__config_path__ = "./alembic.ini"
+__migration_path__ = "./alembic/"
+
+cfg = Config(__config_path__)
+cfg.set_main_option("script_location", __migration_path__)
 
 
 class Database:
@@ -18,9 +27,27 @@ class Database:
             logger.error("Failed to initialize database: no DATABASE_URL defined in env.")
             sys.exit(1)
         self.initialized = False
-        self.engine = AsyncEngine(create_engine(os.getenv("DATABASE_URL")))
+
+        self.DB_URL = os.getenv("DATABASE_URL")
+        self.sync_engine = create_engine(self.DB_URL.replace("asyncpg", "psycopg2"))
+        self.engine = AsyncEngine(create_engine(self.DB_URL))
+        self.run_migration()
+
         self.initialized = True
         logger.success("Database instance initialized.")
+
+    @staticmethod
+    def _execute_upgrade(connection):
+        cfg.attributes["connection"] = connection
+        command.upgrade(cfg, "head")
+
+    def run_migration(self):
+        logger.info("Running database migrations.")
+        if not database_exists(self.sync_engine.url):
+            logger.debug("Creating database.")
+            create_database(self.sync_engine.url)
+        self._execute_upgrade(self.sync_engine)
+        logger.success("Database migrations completed successfully.")
 
     async def get_session(self) -> AsyncSession:
         if not self.initialized:
