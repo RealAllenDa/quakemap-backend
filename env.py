1b85fd0780013864b50c48c844d737ce1a7baba8
RealAllenDa <2153867950@qq.com>
2024-01-29 22:52:41 +0800

    Chore: Bump version to 3.0.0 :)

--- env.py
+++ env.py
@@ -17,7 +17,7 @@ class _Env:
     """
 
     def __init__(self) -> None:
-        self.version = "2.0.6"
+        self.version = "3.0.0"
         self._run_env = None
         self._init_time = None
         self._config = None
